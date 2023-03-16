e530f36628aa3a5f21e909dc51cea07f6128058d
Anastasiya Riabova <vereten.o@yandex.ru>
2023-03-16 20:51:21 +0400

    add .gitignore

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
         double mouthPaymentTwo = service.calculate(9.99, 1_000_000, 24); // ежемесячный платеж
         double mouthPaymentThree = service.calculate(9.99, 1_000_000, 36); // ежемесячный платеж
 
-        System.out.println("Ежемесячный платеж на 12 месяцев : " + mouthPaymentOne);
-        System.out.println("Ежемесячный платеж на 24 месяца : " + mouthPaymentTwo);
-        System.out.println("Ежемесячный платеж на 36 месяцев : " + mouthPaymentThree);
+        System.out.println("Ежемесячный платеж на 12 месяцев: " + mouthPaymentOne);
+        System.out.println("Ежемесячный платеж на 24 месяца: " + mouthPaymentTwo);
+        System.out.println("Ежемесячный платеж на 36 месяцев: " + mouthPaymentThree);
     }
 }
