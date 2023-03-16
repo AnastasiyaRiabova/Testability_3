11afb45fda89ae513c1e2f8e3b436eb4bd5c4498
Anastasiya Riabova <vereten.o@yandex.ru>
2023-03-16 20:50:29 +0400

    add .gitignore

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ public class Main {
 
         CreditPaymentService service = new CreditPaymentService();
         //double interestRate = 9.99; // процентная ставка в процентах
-       // int amountCredit = 1_000_000; // сумма кредита
-       // int creditTerm = 12 ; // срок кредита в месяцах
-        double mouthPaymentOne = service.calculate(9.99 ,1_000_000,12); // ежемесячный платеж
+        // int amountCredit = 1_000_000; // сумма кредита
+        // int creditTerm = 12 ; // срок кредита в месяцах
+        double mouthPaymentOne = service.calculate(9.99, 1_000_000, 12); // ежемесячный платеж
+        double mouthPaymentTwo = service.calculate(9.99, 1_000_000, 24); // ежемесячный платеж
+        double mouthPaymentThree = service.calculate(9.99, 1_000_000, 36); // ежемесячный платеж
+
         System.out.println("Ежемесячный платеж на 12 месяцев : " + mouthPaymentOne);
-        double mouthPaymentTwo = service.calculate(9.99 ,1_000_000,24); // ежемесячный платеж
         System.out.println("Ежемесячный платеж на 24 месяца : " + mouthPaymentTwo);
-        double mouthPaymentThree = service.calculate(9.99 ,1_000_000,36); // ежемесячный платеж
         System.out.println("Ежемесячный платеж на 36 месяцев : " + mouthPaymentThree);
     }
 }
